21bc5328d5a23622eb6dc571c00a798da09be227
Abhishek Kummar Sharma <89777784+abhidigiworld@users.noreply.github.com>
2022-04-20 09:05:32 +0530

    Add files via upload

---
+++ assets/javascript/notes.js
@@ -0,0 +1,143 @@
+        // script for the div to hide and show
+        function mydiv() {
+            var x = document.getElementById("div1");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv2() {
+            var y = document.getElementById("div2");
+            if (y.style.display === "none") {
+                y.style.display = "block";
+            } else {
+                y.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv3() {
+            var a = document.getElementById("div3");
+            if (a.style.display === "none") {
+                a.style.display = "block";
+            } else {
+                a.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv4() {
+            var b = document.getElementById("div4");
+            if (b.style.display === "none") {
+                b.style.display = "block";
+            } else {
+                b.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv5() {
+            var c = document.getElementById("div5");
+            if (c.style.display === "none") {
+                c.style.display = "block";
+            } else {
+                c.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv6() {
+            var d = document.getElementById("div6");
+            if (d.style.display === "none") {
+                d.style.display = "block";
+            } else {
+                d.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv7() {
+            var e = document.getElementById("div7");
+            if (e.style.display === "none") {
+                e.style.display = "block";
+            } else {
+                e.style.display = "none";
+            }
+        };
+
+        // script for the div to hide and show
+        function mydiv8() {
+            var f = document.getElementById("div8");
+            if (f.style.display === "none") {
+                f.style.display = "block";
+            } else {
+                f.style.display = "none";
+            }
+        };
+
+
+
+
+        // this for the mcq part of the div show and hide
+        var ece = document.getElementById("flow");
+
+        function activate() {
+            if (ece.style.display === "none") {
+                ece.style.display = "block";
+            } else {
+                ece.style.display = "none";
+            }
+        };
+
+        var ece1 = document.getElementById("flow1");
+
+        function activate1() {
+            if (ece1.style.display === "none") {
+                ece1.style.display = "block";
+            } else {
+                ece1.style.display = "none";
+            }
+        };
+
+        var ece2 = document.getElementById("flow2");
+
+        function activate2() {
+            if (ece2.style.display === "none") {
+                ece2.style.display = "block";
+            } else {
+                ece2.style.display = "none";
+            }
+        };
+
+        var ece3 = document.getElementById("flow3");
+
+        function activate3() {
+            if (ece3.style.display === "none") {
+                ece3.style.display = "block";
+            } else {
+                ece3.style.display = "none";
+            }
+        };
+
+        var ece4 = document.getElementById("flow4");
+
+        function activate4() {
+            if (ece4.style.display === "none") {
+                ece4.style.display = "block";
+            } else {
+                ece4.style.display = "none";
+            }
+        };
+
+        var ece5 = document.getElementById("flow5");
+
+        function activate5() {
+            if (ece5.style.display === "none") {
+                ece5.style.display = "block";
+            } else {
+                ece5.style.display = "none";
+            }
+        };
